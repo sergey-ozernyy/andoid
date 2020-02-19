545c2607561820ed29c060bfb379ce0a729154b9
sergey-ozernyy <50018166+sergey-ozernyy@users.noreply.github.com>
2020-02-19 10:25:12 +0300

    Create kotlin_for_proffesionals_TypeIntro.kt

---
+++ kotlin_for_proffesionals_TypeIntro.kt
@@ -0,0 +1,15 @@
+const val MAX_EXPERIENCE: Int = 5000
+
+fun main(args: Arrey<String>){
+    val playerName = "Estragon"
+		var experiencePoints = 5
+    var hasSteed = false
+    val tavernName = "Pony"
+    val nameTrack = "Bob"
+    var coins = 50
+    val drincksMenu = "v","a","b"
+    experiencePoints += 5
+		println(experiencePoints)
+    println(playerName)
+    println(playerName.reversed())
+}
